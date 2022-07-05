f615bf4dc63b630730aa6955834ba5ce9e0f093c
hoangvietonline <hoangvietqlr@gmail.com>
2022-07-05 21:13:53 +0700

    fix :ui category screen use girdview

--- lib/domain/model/category_model.dart
+++ lib/domain/model/category_model.dart
@@ -17,27 +17,29 @@ extension CategoryModelExtension on CategoryModel {
       CategoryModel(
           name: 'Balcony repair',
           price: '\$24',
-          imageDescription: AppImages.imgCategoryOne.widget()),
+          imageDescription: AppImages.imgCategoryOne.widget(fit: BoxFit.cover)),
       CategoryModel(
           name: 'Redecorating',
           price: '\$24',
-          imageDescription: AppImages.imgCategoryTwo.widget()),
+          imageDescription: AppImages.imgCategoryTwo.widget(fit: BoxFit.cover)),
       CategoryModel(
           name: 'Painting works',
           price: '\$24',
-          imageDescription: AppImages.imgCategoryThree.widget()),
+          imageDescription:
+              AppImages.imgCategoryThree.widget(fit: BoxFit.cover)),
       CategoryModel(
           name: 'Interior design',
           price: '\$24',
-          imageDescription: AppImages.imgCategoryFour.widget()),
+          imageDescription:
+              AppImages.imgCategoryFour.widget(fit: BoxFit.cover)),
       CategoryModel(
           name: 'Interior design',
           price: '\$24',
-          imageDescription: AppImages.imgCategorFive.widget()),
+          imageDescription: AppImages.imgCategorFive.widget(fit: BoxFit.cover)),
       CategoryModel(
           name: 'Balcony repair',
           price: '\$24',
-          imageDescription: AppImages.imgCategorySix.widget()),
+          imageDescription: AppImages.imgCategorySix.widget(fit: BoxFit.cover)),
     ];
   }
 }
--- lib/presentation/category/category_route.dart
+++ lib/presentation/category/category_route.dart
@@ -1,3 +1,6 @@
+import 'package:base_ui_flutter_example/utils/commom/app_color.dart';
+import 'package:base_ui_flutter_example/utils/commom/app_text.dart';
+import 'package:base_ui_flutter_example/utils/widget/app_back_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -25,25 +28,64 @@ class CategoryState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: categoryList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: [
-                _buildItemCategoryWidget(context, categoryList[index])
-              ],
-            );
-          }),
-    );
+        child: Column(
+      children: [
+        const AppBackWidget(
+          title: 'Interior design',
+        ),
+        SizedBox(
+          height: 16.h,
+        ),
+        Expanded(
+          flex: 1,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10.w,
+                mainAxisSpacing: 10.w),
+            itemCount: categoryList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _buildItemCategoryWidget(context, categoryList[index]);
+            },
+          ),
+        )
+      ],
+    )
+
+        // child: ListView.builder(
+        //     padding: const EdgeInsets.all(8),
+        //     itemCount: categoryList.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       return Column(
+        //         children: [
+        //           _buildItemCategoryWidget(context, categoryList[index])
+        //         ],
+        //       );
+        //     }),
+        );
   }
 
   Widget _buildItemCategoryWidget(
       BuildContext context, CategoryModel categoryModel) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.w),
-      child: Expanded(
-        child: categoryModel.imageDescription ?? const SizedBox(),
+      child: Column(
+        children: [
+          Expanded(
+            child: GridTile(
+              child: categoryModel.imageDescription ?? const SizedBox(),
+            ),
+          ),
+          AppText.primary(
+            categoryModel.name ?? "",
+            fontSize: 16.sp,
+          ),
+          AppText.bodyMedium(
+            categoryModel.price ?? "",
+            fontSize: 14.sp,
+            color: AppColors.gray,
+          )
+        ],
       ),
     );
   }
